74ed62114077a22f53a7bcc64ddcef8f7b9caaac
Andrés González <gandresto@outlook.com>
2020-05-30 19:15:56 -0500

    Agrego función para retornar enunciados de mongo

--- ProyectoFinal/helpers/boletines.py
+++ ProyectoFinal/helpers/boletines.py
@@ -1,7 +1,79 @@
 from pymongo import MongoClient
 # from pprint import pprint
 
-def get_sentences(mongo_uri : str, ):
+def sentences(mongo_uri: str):
+    """
+    Devuelve un diccionario con todas las sentencias en encabezados, 
+    sumarios y contenido de todos los boletines en el corpus. También,
+    devuelve estadísticas de promedio, máximo, mínimo y desviación estándar.
+
+    {
+        'sentences' : <lista con todas las sentencias>,
+        'min': <tamaño mínimo de sentencias en número de tokens>,
+        'max': <tamaño máximo de sentencias en número de tokens>,
+        'avg': <tamaño promedio de sentencias en número de tokens>,
+        'std': <desviación estándar de sentencias en número de tokens>,
+    } 
+
+    :param mongo_uri: La url de conexión a MongoDB
+    :returns: Un diccionario con oraciones por candidato.
+    """
+    with MongoClient(mongo_uri) as client:
+        if not client:
+            raise ConnectionError
+        stats_freeling = client['boletines_db']['stats_freeling']
+        result = stats_freeling.aggregate([
+        {
+            '$project': {
+                'sentences': {
+                    '$concatArrays': [
+                        {
+                            '$map': {
+                                'input': '$encabezado', 
+                                'as': 'sent', 
+                                'in': '$$sent.token'
+                            }
+                        }, {
+                            '$map': {
+                                'input': '$sumarios', 
+                                'as': 'sent', 
+                                'in': '$$sent.token'
+                            }
+                        }, {
+                            '$map': {
+                                'input': '$boletin', 
+                                'as': 'sent', 
+                                'in': '$$sent.token'
+                            }
+                        }
+                    ]
+                }
+            }
+        }, {
+            '$unwind': { 'path': '$sentences' }
+        }, {
+            '$addFields': { 'sent_len': { '$size': '$sentences' } }
+        }, {
+            '$match': { 'sent_len': { '$gte': 5 } }
+        }, {
+            '$group': {
+                '_id': None, 
+                'sentences': { '$push': '$sentences' }, 
+                'count': { '$sum': 1 }, 
+                'avg': { '$avg': '$sent_len' }, 
+                'std': { '$stdDevSamp': '$sent_len' }, 
+                'max': { '$max': '$sent_len' }, 
+                'min': { '$min': '$sent_len' }
+            }
+        }, {
+            '$limit': 1
+        }
+    ])
+    for doc in result:
+        return doc 
+
+
+def sentences_candidato(mongo_uri: str):
     """
     Devuelve un diccionario con todas las sentencias en encabezados, 
     sumarios y contenido de todos los boletines en el corpus.
