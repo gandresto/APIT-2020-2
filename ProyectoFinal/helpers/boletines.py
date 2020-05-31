229cd13be058e6d4dbfbc9cc14620d0f9d1c3449
Andrés González <gandresto@outlook.com>
2020-05-30 21:47:21 -0500

    Método para obtener oraciones desde corenlp

--- ProyectoFinal/helpers/boletines.py
+++ ProyectoFinal/helpers/boletines.py
@@ -1,7 +1,155 @@
 from pymongo import MongoClient
 # from pprint import pprint
 
-def sentences(mongo_uri: str):
+pipeline_sentences_freeling = [
+    {
+        '$project': {
+            'sentences': {
+                '$concatArrays': [
+                    {
+                        '$map': {
+                            'input': '$encabezado', 
+                            'as': 'sent', 
+                            'in': '$$sent.token'
+                        }
+                    }, {
+                        '$map': {
+                            'input': '$sumarios', 
+                            'as': 'sent', 
+                            'in': '$$sent.token'
+                        }
+                    }, {
+                        '$map': {
+                            'input': '$boletin', 
+                            'as': 'sent', 
+                            'in': '$$sent.token'
+                        }
+                    }
+                ]
+            }
+        }
+    }, {
+        '$unwind': { 'path': '$sentences' }
+    }, {
+        '$addFields': { 'sent_len': { '$size': '$sentences' } }
+    }, {
+        '$match': { 'sent_len': { '$gte': 5 } }
+    }, {
+        '$group': {
+            '_id': None, 
+            'sentences': { '$push': '$sentences' }, 
+            'count': { '$sum': 1 }, 
+            'avg': { '$avg': '$sent_len' }, 
+            'std': { '$stdDevSamp': '$sent_len' }, 
+            'max': { '$max': '$sent_len' }, 
+            'min': { '$min': '$sent_len' }
+        }
+    }, {
+        '$limit': 1
+    }
+]
+
+pipeline_sentences_corenlp = [
+    {
+        '$project': {
+            'sentences': {
+                '$concatArrays': [
+                    '$boletin.tokens', '$encabezado.tokens', '$sumarios.tokens'
+                ]
+            }
+        }
+    }, {
+        '$project': {
+            'sentences': {
+                '$map': {
+                    'input': '$sentences', 
+                    'as': 'sent', 
+                    'in': {
+                        '$map': {
+                            'input': '$$sent', 
+                            'as': 'token', 
+                            'in': {
+                                '$arrayElemAt': [
+                                    '$$token', 0
+                                ]
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }, {
+        '$unwind': {
+            'path': '$sentences'
+        }
+    }, {
+        '$addFields': {
+            'num_tokens': {
+                '$size': '$sentences'
+            }
+        }
+    }, {
+        '$project': {
+            'sentence': {
+                '$map': {
+                    'input': '$sentences', 
+                    'as': 'token', 
+                    'in': '$$token.text'
+                }
+            }, 
+            'num_tokens': 1
+        }
+    }, {
+        '$match': {
+            '$or': [
+                {
+                    'num_tokens': {
+                        '$gt': 5
+                    }
+                }, {
+                    '$and': [
+                        {
+                            'sentence': {
+                                '$nin': [
+                                    'Agenda', 'AGENDA', 'agenda'
+                                ]
+                            }
+                        }, {
+                            'num_tokens': {
+                                '$lte': 5, 
+                                '$gt': 2
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    }, {
+        '$group': {
+            '_id': None, 
+            'sentences': {
+                '$push': '$sentence'
+            }, 
+            'count': {
+                '$sum': 1
+            }, 
+            'min': {
+                '$min': '$num_tokens'
+            }, 
+            'max': {
+                '$max': '$num_tokens'
+            }, 
+            'avg': {
+                '$avg': '$num_tokens'
+            }, 
+            'std': {
+                '$stdDevSamp': '$num_tokens'
+            }
+        }
+    }
+]
+
+def sentences(mongo_uri: str, source='freeling'):
     """
     Devuelve un diccionario con todas las sentencias en encabezados, 
     sumarios y contenido de todos los boletines en el corpus. También,
@@ -16,61 +164,23 @@ def sentences(mongo_uri: str):
     } 
 
     :param mongo_uri: La url de conexión a MongoDB
+    :param source: El destino de los enunciados. Pueden se de 'corenlp'
+    o de 'freeling'
     :returns: Un diccionario con oraciones por candidato.
     """
     with MongoClient(mongo_uri) as client:
         if not client:
             raise ConnectionError
-        stats_freeling = client['boletines_db']['stats_freeling']
-        result = stats_freeling.aggregate([
-        {
-            '$project': {
-                'sentences': {
-                    '$concatArrays': [
-                        {
-                            '$map': {
-                                'input': '$encabezado', 
-                                'as': 'sent', 
-                                'in': '$$sent.token'
-                            }
-                        }, {
-                            '$map': {
-                                'input': '$sumarios', 
-                                'as': 'sent', 
-                                'in': '$$sent.token'
-                            }
-                        }, {
-                            '$map': {
-                                'input': '$boletin', 
-                                'as': 'sent', 
-                                'in': '$$sent.token'
-                            }
-                        }
-                    ]
-                }
-            }
-        }, {
-            '$unwind': { 'path': '$sentences' }
-        }, {
-            '$addFields': { 'sent_len': { '$size': '$sentences' } }
-        }, {
-            '$match': { 'sent_len': { '$gte': 5 } }
-        }, {
-            '$group': {
-                '_id': None, 
-                'sentences': { '$push': '$sentences' }, 
-                'count': { '$sum': 1 }, 
-                'avg': { '$avg': '$sent_len' }, 
-                'std': { '$stdDevSamp': '$sent_len' }, 
-                'max': { '$max': '$sent_len' }, 
-                'min': { '$min': '$sent_len' }
-            }
-        }, {
-            '$limit': 1
-        }
-    ])
-    for doc in result:
-        return doc 
+        
+        if source=='freeling':    
+            stats_freeling = client['boletines_db']['stats_freeling']
+            result = stats_freeling.aggregate(pipeline_sentences_freeling)
+        elif source=='corenlp':
+            stats_freeling = client['boletines_db']['stats_corenlp']
+            result = stats_freeling.aggregate(pipeline_sentences_corenlp)
+
+        for doc in result:
+            return doc 
 
 
 def sentences_candidato(mongo_uri: str):
